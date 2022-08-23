ede82eb8ca108c09ea0d44601a71de34513122a0
Braxton Zane Massengale <bmassengale@uoc.edu>
2022-08-23 13:26:44 +0200

    Configured announcement content type

--- pages/announcements/index.vue
+++ pages/announcements/index.vue
@@ -2,7 +2,7 @@
   <main>
     <section v-if="posts" class="w-full max-w-5xl mx-auto">
       <h1 class="title">Announcements</h1>
-      <posts post-type="blog" :amount="10" />
+      <posts post-type="announcements" :amount="10" />
     </section>
   </main>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -6,8 +6,8 @@
     </section>
 
     <section class="mt-8">
-      <h3 class="max-w-5xl mx-auto text-primary-600 dark:text-primary-400">Latest blog post</h3>
-      <posts post-type="blog" :amount="1" />
+      <h3 class="max-w-5xl mx-auto text-primary-600 dark:text-primary-400">Latest announcements</h3>
+      <posts post-type="announcements" :amount="1" />
     </section>
   </main>
 </template>
